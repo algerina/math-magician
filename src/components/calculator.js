d10d14778e53f1f577002f7edd26b3fbd2acd85a
Amel Khiri <53347966+algerina@users.noreply.github.com>
2021-11-30 23:32:07 +0100

    Fix linter errors

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -7,57 +7,23 @@ export default function Calculator() {
       <div className="screen">0</div>
       <button type="button" className="number">AC</button>
       <button type="button" className="number">+/-</button>
-      <button type="button" className="number">
-        %
-      </button>
-      <button type="button" className="operation">
-        ÷
-      </button>
-      <button type="button" className="number">
-        7
-      </button>
-      <button type="button" className="number">
-        8
-      </button>
-      <button type="button" className="number">
-        9
-      </button>
-      <button type="button" className="operation">
-        X
-      </button>
-      <button type="button" className="number">
-        4
-      </button>
-      <button type="button" className="number">
-        5
-      </button>
-      <button type="button" className="number">
-        6
-      </button>
-      <button type="button" className="operation">
-        -
-      </button>
-      <button type="button" className="number">
-        1
-      </button>
-      <button type="button" className="number">
-        2
-      </button>
-      <button type="button" className="number">
-        3
-      </button>
-      <button type="button" className="operation">
-        +
-      </button>
-      <button type="button" className="large-button">
-        0
-      </button>
-      <button type="button" className="number">
-        .
-      </button>
-      <button type="button" className="operation">
-        =
-      </button>
-    </div>
+      <button type="button" className="number">%</button>
+      <button type="button" className="operation">÷</button>
+      <button type="button" className="number">7</button>
+      <button type="button" className="number">8</button>
+      <button type="button" className="number">9</button>
+      <button type="button" className="operation">X</button>
+      <button type="button" className="number">4</button>
+      <button type="button" className="number">5</button>
+      <button type="button" className="number">6</button>
+      <button type="button" className="operation">-</button>
+      <button type="button" className="number">1</button>
+      <button type="button" className="number">2</button>
+      <button type="button" className="number">3</button>
+      <button type="button" className="operation">+</button>
+      <button type="button" className="large-button">0</button>
+      <button type="button" className="number">.</button>
+      <button type="button" className="operation">=</button>
+    </div> 
   );
 }
