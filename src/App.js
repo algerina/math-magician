7dc61c916a9ea6009ab0f29618bc49fb5c552fea
Amel Khiri <53347966+algerina@users.noreply.github.com>
2021-12-02 20:50:21 +0100

    Fix linter error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Calculator from './components/Calculator.js';
+import Calculator from './components/Calculator';
 
 // import render from 'dom-serializer';
 
